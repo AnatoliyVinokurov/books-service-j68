eef6724415f5d0a083e6f9eabc11fb5757d8057b
AnatoliyVinokurov <anatolij.isit@gmail.com>
2023-10-30 17:27:09 +0200

    first commit

---
+++ src/main/java/telran/java48/BooksServiceJ68Application.java
@@ -0,0 +1,13 @@
+package telran.java48;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksServiceJ68Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksServiceJ68Application.class, args);
+	}
+
+}
