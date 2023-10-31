dabe8e5e7e942cdd63d92f1e90bcd1dc6be4614a
AnatoliyVinokurov <anatolij.isit@gmail.com>
2023-10-31 18:48:25 +0200

    config

--- src/main/java/telran/java48/BookServiceApplication.java
+++ src/main/java/telran/java48/BookServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BooksServiceJ68Application {
+public class BookServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BooksServiceJ68Application.class, args);
+		SpringApplication.run(BookServiceApplication.class, args);
 	}
 
 }
